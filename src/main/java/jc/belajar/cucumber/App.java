74d8a6519daa6092759b254201a3776187482144
meimanhulu <94421861+meimanhulu@users.noreply.github.com>
2025-01-31 21:14:53 +0700

    Initial commit

---
+++ src/main/java/jc/belajar/cucumber/App.java
@@ -0,0 +1,13 @@
+package jc.belajar.cucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
